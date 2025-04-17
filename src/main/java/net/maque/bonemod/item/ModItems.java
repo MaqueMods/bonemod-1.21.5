8fb0d518586508d60708b5ab690315a34102999b
jack <jackrgartlan+1@gmail.com>
2025-04-17 15:35:18 +1000

    added bone shard texture

---
+++ src/main/java/net/maque/bonemod/item/ModItems.java
@@ -0,0 +1,27 @@
+package net.maque.bonemod.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.maque.bonemod.Bonemod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item BONE_SHARD = registerItem("bone_shard", new Item(new Item.Settings()));
+    public static final Item BONE_TOMAHAWK = registerItem("bone_tomahawk", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(Bonemod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        Bonemod.LOGGER.info("Registering Mod Items for " + Bonemod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(entries -> {
+            entries.add(BONE_SHARD);
+            entries.add(BONE_TOMAHAWK);
+        });
+    }
+}
