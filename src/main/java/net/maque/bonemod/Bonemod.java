1f173e61676b794503f0d0c16ef055f07927942a
jack <jackrgartlan+1@gmail.com>
2025-04-17 15:55:25 +1000

    fixed bone shard texture

--- src/main/java/net/maque/bonemod/Bonemod.java
+++ src/main/java/net/maque/bonemod/Bonemod.java
@@ -2,6 +2,7 @@ package net.maque.bonemod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.maque.bonemod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,5 +12,6 @@ public class Bonemod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
 	}
 }
--- src/main/java/net/maque/bonemod/item/ModItems.java
+++ src/main/java/net/maque/bonemod/item/ModItems.java
@@ -6,22 +6,20 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-    public static final Item BONE_SHARD = registerItem("bone_shard", new Item(new Item.Settings()));
-    public static final Item BONE_TOMAHAWK = registerItem("bone_tomahawk", new Item(new Item.Settings()));
-
+    public static final Item BONE_SHARD=registerItem("bone_shard",new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Bonemod.MOD_ID,"bone_shard")))));
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(Bonemod.MOD_ID, name), item);
     }
 
     public static void registerModItems() {
         Bonemod.LOGGER.info("Registering Mod Items for " + Bonemod.MOD_ID);
-
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(entries -> {
             entries.add(BONE_SHARD);
-            entries.add(BONE_TOMAHAWK);
         });
     }
 }
